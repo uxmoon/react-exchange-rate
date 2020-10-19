5e0e3d0dbfaa27ec38fbe756ade0787be28a794b
Martin Luna <uxmoon@gmail.com>
2020-10-19 15:59:32 -0300

    Rates - updated flags styles to match design

--- src/components/RatesItem.js
+++ src/components/RatesItem.js
@@ -5,12 +5,19 @@ class RatesItem extends Component {
     return (
       <div className="Rates-item">
         {/* <div>{this.props.flag}</div> */}
-        <div><img src={`https://www.countryflags.io/${this.props.flag}/flat/16.png`} />
+        <div>
+          <span>
+          <img
+            src={`https://www.countryflags.io/${this.props.flag}/flat/48.png`}
+            alt=""
+            aria-hidden="true"
+          />
+          </span>
         </div>
         <div>{this.props.currency}</div>
         <div>{this.props.num}</div>
       </div>
-    )
+    );
   }
 }
 
