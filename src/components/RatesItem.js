03be61ed54c44bd69c22e52ab7e83fae8c552a0f
Martin Luna <uxmoon@gmail.com>
2020-10-19 15:45:09 -0300

    Rates - added flag using countryflag.io

--- src/components/RatesItem.js
+++ src/components/RatesItem.js
@@ -4,7 +4,9 @@ class RatesItem extends Component {
   render() {
     return (
       <div className="Rates-item">
-        <div>{this.props.flag}</div>
+        {/* <div>{this.props.flag}</div> */}
+        <div><img src={`https://www.countryflags.io/${this.props.flag}/flat/16.png`} />
+        </div>
         <div>{this.props.currency}</div>
         <div>{this.props.num}</div>
       </div>
