a645bed85e5c96f17c2992b54c7b24f6ee2e0032
Martin Luna <uxmoon@gmail.com>
2020-10-19 11:51:56 -0300

    Updated layout for App and updated styles

--- src/App.js
+++ src/App.js
@@ -1,56 +1,58 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import Button from './components/Button';
 
 function App() {
   return (
-    <div>
-      <h1>Histórico de cotizaciones</h1>
+    <div className="App">
+      <div className="App-container">
+        <h1 className="App-title">Histórico de cotizaciones</h1>
 
-      <form>
+        <form>
+          <div>
+            <label htmlFor="currency">Selecciona la moneda de referencia</label>
+            <select id="currency">
+              <option value="">Moneda</option>
+              <option value="">Moneda</option>
+              <option value="">Moneda</option>
+              <option value="">Moneda</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="date">Ingresá la fecha de cotización</label>
+            <input type="text" placeholder="DD / MM / YYYY" id="date" />
+          </div>
+
+          <Button label="Buscar cotizaciones" color="primary" />
+        </form>
+
         <div>
-          <label>Selecciona la moneda de referencia</label>
-          <select>
-            <option value="">Moneda</option>
-            <option value="">Moneda</option>
-            <option value="">Moneda</option>
-            <option value="">Moneda</option>
-          </select>
+          <div>
+            <div>flag</div>
+            <div>Moneda</div>
+            <div>Valor</div>
+          </div>
+          <div>
+            <div>flag</div>
+            <div>Moneda</div>
+            <div>Valor</div>
+          </div>
+          <div>
+            <div>flag</div>
+            <div>Moneda</div>
+            <div>Valor</div>
+          </div>
+          <div>
+            <div>flag</div>
+            <div>Moneda</div>
+            <div>Valor</div>
+          </div>
         </div>
 
         <div>
-          <label>Ingresá la fecha de cotización</label>
-          <input type="text" placeholder="DD / MM / YYYY" />
+          <Button label="Ver más cotizaciones" color="secondary" />
         </div>
-
-        <Button label="Buscar cotizaciones" color="primary" />
-      </form>
-
-      <div>
-        <div>
-          <div>flag</div>
-          <div>Moneda</div>
-          <div>Valor</div>
-        </div>
-        <div>
-          <div>flag</div>
-          <div>Moneda</div>
-          <div>Valor</div>
-        </div>
-        <div>
-          <div>flag</div>
-          <div>Moneda</div>
-          <div>Valor</div>
-        </div>
-        <div>
-          <div>flag</div>
-          <div>Moneda</div>
-          <div>Valor</div>
-        </div>
-      </div>
-
-      <div>
-        <Button label="Ver más cotizaciones" color="secondary" />
       </div>
     </div>
   );
