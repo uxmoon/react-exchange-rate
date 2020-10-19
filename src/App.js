0e86a4447b536af6bd9e448248697c92def95ea7
Martin Luna <uxmoon@gmail.com>
2020-10-19 11:00:04 -0300

    added form markup base

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ function App() {
   return (
     <div>
       <h1>Histórico de cotizaciones</h1>
+
+      <form>
+        <div>
+          <label>Selecciona la moneda de referencia</label>
+          <select>
+            <option value="">Moneda</option>
+            <option value="">Moneda</option>
+            <option value="">Moneda</option>
+            <option value="">Moneda</option>
+          </select>
+        </div>
+
+        <div>
+          <label>Ingresá la fecha de cotización</label>
+          <input type="text" placeholder="DD / MM / YYYY" />
+        </div>
+
+        <button>Buscar cotizaciones</button>
+      </form>
     </div>
   );
 }
