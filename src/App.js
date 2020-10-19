c6f76423a081fb6644407b00591309711eb1c04c
Martin Luna <uxmoon@gmail.com>
2020-10-19 13:06:00 -0300

    Added axios and console.log response from exchange rates API

--- src/App.js
+++ src/App.js
@@ -1,65 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import './Form.scss';
 import './Rates.scss';
 import Button from './components/Button';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-container">
-        <h1 className="App-title">Histórico de cotizaciones</h1>
+class App extends Component {
 
-        <form className="Form">
-          <div className="Form-field">
-            <label htmlFor="currency">Selecciona la moneda de referencia</label>
-            <div className="Form-select">
-              <select id="currency">
-                <option value="">Moneda</option>
-                <option value="">Moneda</option>
-                <option value="">Moneda</option>
-                <option value="">Moneda</option>
-              </select>
+  componentDidMount() {
+    axios.get('https://api.exchangeratesapi.io/latest')
+    .then((response) => {
+      console.log(response)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-container">
+          <h1 className="App-title">Histórico de cotizaciones</h1>
+
+          <form className="Form">
+            <div className="Form-field">
+              <label htmlFor="currency">
+                Selecciona la moneda de referencia
+              </label>
+              <div className="Form-select">
+                <select id="currency">
+                  <option value="">Moneda</option>
+                  <option value="">Moneda</option>
+                  <option value="">Moneda</option>
+                  <option value="">Moneda</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="Form-field">
+              <label htmlFor="date">Ingresá la fecha de cotización</label>
+              <input type="text" placeholder="DD / MM / YYYY" id="date" />
+            </div>
+
+            <Button label="Buscar cotizaciones" color="primary" />
+          </form>
+
+          <div className="Rates">
+            <div className="Rates-item">
+              <div>flag</div>
+              <div>Moneda</div>
+              <div>Valor</div>
+            </div>
+            <div className="Rates-item">
+              <div>flag</div>
+              <div>Moneda</div>
+              <div>Valor</div>
+            </div>
+            <div className="Rates-item">
+              <div>flag</div>
+              <div>Moneda</div>
+              <div>Valor</div>
+            </div>
+            <div className="Rates-item">
+              <div>flag</div>
+              <div>Moneda</div>
+              <div>Valor</div>
             </div>
           </div>
 
-          <div className="Form-field">
-            <label htmlFor="date">Ingresá la fecha de cotización</label>
-            <input type="text" placeholder="DD / MM / YYYY" id="date" />
+          <div>
+            <Button label="Ver más cotizaciones" color="secondary" />
           </div>
-
-          <Button label="Buscar cotizaciones" color="primary" />
-        </form>
-
-        <div className="Rates">
-          <div className="Rates-item">
-            <div>flag</div>
-            <div>Moneda</div>
-            <div>Valor</div>
-          </div>
-          <div className="Rates-item">
-            <div>flag</div>
-            <div>Moneda</div>
-            <div>Valor</div>
-          </div>
-          <div className="Rates-item">
-            <div>flag</div>
-            <div>Moneda</div>
-            <div>Valor</div>
-          </div>
-          <div className="Rates-item">
-            <div>flag</div>
-            <div>Moneda</div>
-            <div>Valor</div>
-          </div>
-        </div>
-
-        <div>
-          <Button label="Ver más cotizaciones" color="secondary" />
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
