5d53f90e6e2f3d921302802bec4a498714a4b23b
Martin Luna <uxmoon@gmail.com>
2020-10-19 15:21:17 -0300

    Rates - pass state as props and console log default currencies

--- src/components/Rates.js
+++ src/components/Rates.js
@@ -3,14 +3,23 @@ import './Rates.scss';
 import RatesItem from './RatesItem';
 
 class Rates extends Component {
+
+  createRates = () => {
+    const rates = this.props.rates;
+    const defaultCurrencies = ['CAD', 'GBP', 'USD', 'EUR'];
+    const item = [];
+
+    for(let key in rates) {
+      if(rates.hasOwnProperty(key) && defaultCurrencies.includes(key)) {
+        console.log(`currency: ${key}, rate: ${rates[key]} `)
+      }
+    }
+  }
+
   render() {
     return (
       <div className="Rates">
-        <div className="Rates-item">
-          <div>flag</div>
-          <div>Moneda</div>
-          <div>Valor</div>
-        </div>
+        {this.createRates()}
         <RatesItem />
       </div>
     );
--- src/components/RatesItem.js
+++ src/components/RatesItem.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class RatesItem extends Component {
   render() {
     return (
-      <div>
+      <div className="Rates-item">
         <div>flag</div>
+        <div>moneda</div>
+        <div>valor</div>
       </div>
     )
   }
