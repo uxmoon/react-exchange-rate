bd7f4da41ade3196b3a52c5625cbba3599c99810
Martin Luna <uxmoon@gmail.com>
2022-01-22 21:31:53 -0300

    Destructure variable from dotenv

--- functions/fetch-currencies/fetch-currencies.js
+++ functions/fetch-currencies/fetch-currencies.js
@@ -1,10 +1,11 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const axios = require("axios");
+
 const handler = async (event) => {
   const { symbols } = event.queryStringParameters;
 
-  const API_KEY = process.env.VITE_API_KEY;
-  const url = `http://api.exchangeratesapi.io/v1/latest?access_key=${API_KEY}&symbols=${symbols}`;
+  const { VITE_API_KEY } = process.env;
+  const url = `http://api.exchangeratesapi.io/v1/latest?access_key=${VITE_API_KEY}&symbols=${symbols}`;
 
   try {
     const { data } = await axios.get(url);
